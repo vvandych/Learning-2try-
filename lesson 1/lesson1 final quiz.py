0eeb285b5fa218a5974f09772df3b1959b9138dd
vvandych <vandychvolodymyr@gmail.com>
2021-09-13 20:57:32 +0300

    Create lesson1 final quiz.py

---
+++ lesson 1/lesson1 final quiz.py
@@ -0,0 +1,20 @@
+#1 lesson final quiz
+# Write Python code that assigns to the 
+# variable url a string that is the value 
+# of the first URL that appears in a link 
+# tag in the string page.
+# Your code should print http://udacity.com
+# Make sure that if page were changed to
+
+# page = '<a href="http://udacity.com">Hello world</a>'
+
+# that your code still assigns the same value to the variable 'url', 
+# and therefore still prints the same thing.
+
+# page = contents of a web page
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+start_link = page.find('<a href=')
+end_link=page.find('>',start_link)
+url=page[start_link+9:end_link-1]
+print (url)
